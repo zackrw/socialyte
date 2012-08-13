d6623b373cc5264911359cbb91e0182cde8ec16e
Brandon Smith <brandon.smith@yale.edu>
2012-08-12 21:27:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialyte::Application.config.secret_token = 'c7d22ad06133d933c86cf6c3816c42ab7d65916123fcd5bb32dc1a61a279f221dd1e547def3c835fe52d57d40f00c06b0640ed703a8aa11eeefa30beda106419'
