37472c4974a3c1f62c0e0cda6118e29b2a8e5129
Brandon Smith <brandon.smith@yale.edu>
2012-08-14 02:00:31 -0500

    fixed some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
   
   def self.create_with_omniauth(auth)
-  	create! do |user|
+  create! do |user|
     user.provider = auth["provider"]
     user.uid = auth["uid"]
     user.name = auth["info"]["name"]
+  	end
   end
 end
